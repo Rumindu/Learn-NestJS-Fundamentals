2bf091e16bb1ca19c2b00274a9cfaa62f49a9694
Rumindu De Silva <rumindukavishka14@gmail.com>
2025-09-07 16:34:57 +0530

    Use Route Parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors') // GET /coffees/flavors
   findAllFlavors(): string {
     return 'This action returns all coffee flavors';
+  }
+  @Get(':id') // Dynamic route parameter
+  findOne(@Param('id') id: string): string {
+    return `This action returns coffee #${id}`;
   }
 }
